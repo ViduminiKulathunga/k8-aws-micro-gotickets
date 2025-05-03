3d798bb03d596e6aebd1502b40f9ab9fa13e49e6
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-04 00:50:18 +0200

    GCP k8 deployment skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,9 @@ const app = express();
 app.use(json());
 
 app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  res.send("Hi there Vidu!");
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log("Listening on port 3000!!");
 });

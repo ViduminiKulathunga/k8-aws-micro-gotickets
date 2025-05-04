de9d108b18e021f61150b0b7d783524ccbe4c7de
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-04 19:31:36 +0200

    custom error abstract class implemented

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   error: Error,
@@ -8,13 +7,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (error instanceof RequestValidationError) {
-    return res
-      .status(error.statusCode)
-      .send({ errors: error.serializeError() });
-  }
-
-  if (error instanceof DatabaseConnectionError) {
+  if (error instanceof CustomError) {
     return res
       .status(error.statusCode)
       .send({ errors: error.serializeError() });

f8219f7c297cb76367c2cab978d63ca0f9a1f6e1
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-08 16:47:23 +0200

    useRequest hook and signup page

---
+++ client/hooks/use-request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useRequest = ({ url, method, body }) => {
+  const [errors, setErrors] = useState([]);
+
+  const doRequest = async () => {
+    try {
+      const response = await axios[method](url, body);
+      setErrors([]);
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map((e) => (
+              <li key={e.message}>{e.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
+
+export default useRequest;
---
+++ client/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config) => {
+    return {
+      ...config,
+      watchOptions: {
+        ...config.watchOptions,
+        poll: 300,
+      },
+    };
+  },
+  allowedDevOrigins: ["gotickets.dev"],
+};

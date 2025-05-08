7f2883175a0f1f7380f0f2ec36318ffad9da95a0
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-08 19:54:51 +0200

    Reusable API client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const Landing = ({ currentUser }) => {
   console.log(currentUser);
@@ -7,25 +7,11 @@ const Landing = ({ currentUser }) => {
   return <div>Hello world</div>;
 };
 
-Landing.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const { data } = await axios
-      .get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        }
-      )
-      .catch((err) => console.log(err.message));
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
-    return data;
-  } else {
-    const { data } = await axios
-      .get("/api/users/currentuser")
-      .catch((err) => console.log(err.message));
-
-    return data;
-  }
+  return data;
 };
 
 export default Landing;
